64d6faf20dbc3e8c25d771067f2cec0b03af5e38
crowlkats <crowlkats@toaxl.com>
2024-02-29 15:36:08 +0100

    fmt

--- frontend/components/PackageHit.tsx
+++ frontend/components/PackageHit.tsx
@@ -1,3 +1,4 @@
+// Copyright 2024 the JSR authors. All rights reserved. MIT license.
 import type { OramaPackageHit } from "../util.ts";
 import type { Package } from "../utils/api_types.ts";
 import type { ListDisplayItem } from "./List.tsx";
