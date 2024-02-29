e73c26a607c10b89ffeadca574e67ae421e2e744
crowlkats <crowlkats@toaxl.com>
2024-02-29 22:09:24 +0100

    fix

--- frontend/islands/PollPublishingTask.tsx
+++ frontend/islands/PollPublishingTask.tsx
@@ -1,3 +1,4 @@
+// Copyright 2024 the JSR authors. All rights reserved. MIT license.
 import { IS_BROWSER } from "$fresh/runtime.ts";
 import { useState,useEffect  } from "preact/hooks";
 import { api, path } from "../utils/api.ts";
