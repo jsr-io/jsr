127b956fb031bc8863c34f57b27f9ba7ae96fd64
Luca Casonato <hello@lcas.dev>
2024-03-01 10:14:02 +0100

    fix: add a "Edit on GitHub" link to docs (#93)

--- frontend/dev.ts
+++ frontend/dev.ts
@@ -4,6 +4,4 @@
 import dev from "$fresh/dev.ts";
 import config from "./fresh.config.ts";
 
-Deno.env.set("OTLP_ENDPOINT", "http://localhost:4318");
-
 await dev(import.meta.url, "./main.ts", config);
