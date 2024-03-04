0734feb6f27226e19dc281468b4eb095a6880b29
John Donmoyer <john@deno.com>
2024-03-04 12:39:49 -0500

    fix: adjust styling on provenance statement (#111)
    adjust colors and spacing on provenance statement to match JSR theme
    
    <img width="614" alt="Screenshot 2024-03-01 at 11 38 15 AM"
    src="https://github.com/jsr-io/jsr/assets/776987/6437184b-caa5-4167-8164-a6e57b03c322">
    
    ---------
    
    Co-authored-by: ryu <114303361+ryuapp@users.noreply.github.com>
    Co-authored-by: Shinya Fujino <shf0811@gmail.com>
    Co-authored-by: Roj [roːʒ] <rojvv@icloud.com>
    Co-authored-by: Max Foxie <altfoxie@icloud.com>
    Co-authored-by: Josh Collinsworth <joshuajcollinsworth@gmail.com>
    Co-authored-by: Ryan Dahl <ry@tinyclouds.org>
    Co-authored-by: Leo Kettmeir <crowlkats@toaxl.com>
    Co-authored-by: Bartek Iwańczuk <biwanczuk@gmail.com>
    Co-authored-by: Luca Casonato <hello@lcas.dev>
    Co-authored-by: 三咲智子 Kevin Deng <sxzz@sxzz.moe>
    Co-authored-by: Yutaka Kamei <kamei@yykamei.me>
    Co-authored-by: Halvard Mørstad <accounts@moerstad.no>
    Co-authored-by: tison <wander4096@gmail.com>
    Co-authored-by: Yoshiya Hinosawa <stibium121@gmail.com>
    Co-authored-by: Roj <ez@roj.im>

--- frontend/routes/package/(_components)/Docs.tsx
+++ frontend/routes/package/(_components)/Docs.tsx
@@ -27,41 +27,43 @@ export function DocsView({ docs, params, selectedVersion }: DocsProps) {
       <div class="ddoc" dangerouslySetInnerHTML={{ __html: docs.main }} />
 
       {selectedVersion.rekorLogId && (
-        <div class="mt-4 border border-gray-400 rounded-md py-1 px-2">
-          <div className="items-center">
-            <span className="text-sm text-gray-600 mr-1">
-              Built and signed on
-            </span>
+        <div class="mt-12 border-2 border-jsr-cyan-500 max-w-xl rounded-md py-4 px-6">
+          <div class="flex flex-row items-end justify-between">
+            <div className="items-center">
+              <span className="text-sm text-jsr-gray-300">
+                Built and signed on
+              </span>
 
-            <div className="flex items-center">
-              <svg
-                aria-hidden="true"
-                role="img"
-                class="text-green-600"
-                viewBox="0 0 16 16"
-                width="18"
-                height="18"
-                fill="currentColor"
-                style="display: inline-block; user-select: none; vertical-align: text-bottom;"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M9.585.52a2.678 2.678 0 00-3.17 0l-.928.68a1.178 1.178 0 01-.518.215L3.83 1.59a2.678 2.678 0 00-2.24 2.24l-.175 1.14a1.178 1.178 0 01-.215.518l-.68.928a2.678 2.678 0 000 3.17l.68.928c.113.153.186.33.215.518l.175 1.138a2.678 2.678 0 002.24 2.24l1.138.175c.187.029.365.102.518.215l.928.68a2.678 2.678 0 003.17 0l.928-.68a1.17 1.17 0 01.518-.215l1.138-.175a2.678 2.678 0 002.241-2.241l.175-1.138c.029-.187.102-.365.215-.518l.68-.928a2.678 2.678 0 000-3.17l-.68-.928a1.179 1.179 0 01-.215-.518L14.41 3.83a2.678 2.678 0 00-2.24-2.24l-1.138-.175a1.179 1.179 0 01-.518-.215L9.585.52zM7.303 1.728c.415-.305.98-.305 1.394 0l.928.68c.348.256.752.423 1.18.489l1.136.174c.51.078.909.478.987.987l.174 1.137c.066.427.233.831.489 1.18l.68.927c.305.415.305.98 0 1.394l-.68.928a2.678 2.678 0 00-.489 1.18l-.174 1.136a1.178 1.178 0 01-.987.987l-1.137.174a2.678 2.678 0 00-1.18.489l-.927.68c-.415.305-.98.305-1.394 0l-.928-.68a2.678 2.678 0 00-1.18-.489l-1.136-.174a1.178 1.178 0 01-.987-.987l-.174-1.137a2.678 2.678 0 00-.489-1.18l-.68-.927a1.178 1.178 0 010-1.394l.68-.928c.256-.348.423-.752.489-1.18l.174-1.136c.078-.51.478-.909.987-.987l1.137-.174a2.678 2.678 0 001.18-.489l.927-.68zM11.28 6.78a.75.75 0 00-1.06-1.06L7 8.94 5.78 7.72a.75.75 0 00-1.06 1.06l1.75 1.75a.75.75 0 001.06 0l3.75-3.75z"
+              <div className="flex gap-2 items-center">
+                <span className="text-2xl font-bold">GitHub Actions</span>
+                <svg
+                  aria-hidden="true"
+                  role="img"
+                  class="text-green-600"
+                  viewBox="0 0 16 16"
+                  width="20"
+                  height="20"
+                  fill="currentColor"
+                  style="display: inline-block; user-select: none; vertical-align: text-bottom;"
                 >
-                </path>
-              </svg>
-              <span className="text-lg font-bold ml-1">GitHub Actions</span>
+                  <path
+                    fill-rule="evenodd"
+                    d="M9.585.52a2.678 2.678 0 00-3.17 0l-.928.68a1.178 1.178 0 01-.518.215L3.83 1.59a2.678 2.678 0 00-2.24 2.24l-.175 1.14a1.178 1.178 0 01-.215.518l-.68.928a2.678 2.678 0 000 3.17l.68.928c.113.153.186.33.215.518l.175 1.138a2.678 2.678 0 002.24 2.24l1.138.175c.187.029.365.102.518.215l.928.68a2.678 2.678 0 003.17 0l.928-.68a1.17 1.17 0 01.518-.215l1.138-.175a2.678 2.678 0 002.241-2.241l.175-1.138c.029-.187.102-.365.215-.518l.68-.928a2.678 2.678 0 000-3.17l-.68-.928a1.179 1.179 0 01-.215-.518L14.41 3.83a2.678 2.678 0 00-2.24-2.24l-1.138-.175a1.179 1.179 0 01-.518-.215L9.585.52zM7.303 1.728c.415-.305.98-.305 1.394 0l.928.68c.348.256.752.423 1.18.489l1.136.174c.51.078.909.478.987.987l.174 1.137c.066.427.233.831.489 1.18l.68.927c.305.415.305.98 0 1.394l-.68.928a2.678 2.678 0 00-.489 1.18l-.174 1.136a1.178 1.178 0 01-.987.987l-1.137.174a2.678 2.678 0 00-1.18.489l-.927.68c-.415.305-.98.305-1.394 0l-.928-.68a2.678 2.678 0 00-1.18-.489l-1.136-.174a1.178 1.178 0 01-.987-.987l-.174-1.137a2.678 2.678 0 00-.489-1.18l-.68-.927a1.178 1.178 0 010-1.394l.68-.928c.256-.348.423-.752.489-1.18l.174-1.136c.078-.51.478-.909.987-.987l1.137-.174a2.678 2.678 0 001.18-.489l.927-.68zM11.28 6.78a.75.75 0 00-1.06-1.06L7 8.94 5.78 7.72a.75.75 0 00-1.06 1.06l1.75 1.75a.75.75 0 001.06 0l3.75-3.75z"
+                  >
+                  </path>
+                </svg>
+              </div>
             </div>
-          </div>
 
-          <a
-            href={`https://search.sigstore.dev/?logIndex=${selectedVersion.rekorLogId}`}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-sm text-blue-600 hover:underline"
-          >
-            View transparency log
-          </a>
+            <a
+              href={`https://search.sigstore.dev/?logIndex=${selectedVersion.rekorLogId}`}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-sm link"
+            >
+              View transparency log
+            </a>
+          </div>
         </div>
       )}
     </div>
