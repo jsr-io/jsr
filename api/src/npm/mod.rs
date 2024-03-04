1d5e5f361561c5c4643def24ecf3a1365db85721
Marvin Hagemeister <marvin@deno.com>
2024-03-04 18:46:02 +0100

    chore: bump npm tarball revision

--- api/src/npm/mod.rs
+++ api/src/npm/mod.rs
@@ -25,7 +25,7 @@ pub use self::tarball::NpmTarballOptions;
 pub use self::types::NpmMappedJsrPackageName;
 use self::types::NpmVersionInfo;
 
-pub const NPM_TARBALL_REVISION: u32 = 5;
+pub const NPM_TARBALL_REVISION: u32 = 6;
 
 pub async fn generate_npm_version_manifest<'a>(
   db: &Database,
