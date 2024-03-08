ef0ca0c3a3f3f1a131ca47e3380e2248fa9494c0
Luca Casonato <hello@lcas.dev>
2024-03-08 11:11:19 +0100

    fix: flash of incorrect `Ctrl` value in search box

--- frontend/utils/os.ts
+++ frontend/utils/os.ts
@@ -1,7 +1,8 @@
 // Copyright 2024 the JSR authors. All rights reserved. MIT license.
 
-export function isMacLike(): boolean {
-  return window.navigator.platform.match(/(Mac|iPhone|iPod|iPad)/i)
-    ? true
-    : false;
+import { IS_BROWSER } from "$fresh/runtime.ts";
+
+export function useMacLike(): boolean | undefined {
+  if (!IS_BROWSER) return undefined;
+  return !!window.navigator.platform.match(/(Mac|iPhone|iPod|iPad)/i);
 }
