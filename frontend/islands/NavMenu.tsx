dc89059486d45ecf84bd6038f0a45b67ab25ec6f
ryu <114303361+ryuapp@users.noreply.github.com>
2024-04-09 03:54:10 +0900

    Match current navigation design

--- frontend/islands/NavMenu.tsx
+++ frontend/islands/NavMenu.tsx
@@ -60,7 +60,7 @@ export function NavMenu() {
   return (
     <div
       id="nav-menu"
-      class={`group absolute right-4 md:right-10 rounded border-2 border-jsr-cyan-200 hover:bg-jsr-cyan-50 hover:cursor-pointer ${
+      class={`group absolute right-4 md:right-10 rounded md:rounded-b-none border-1 md:border-b-0 border-jsr-cyan-100 hover:bg-jsr-cyan-50 hover:cursor-pointer ${
         displayMenu ? "" : "hidden"
       }`}
       aria-expanded={open ? "true" : "false"}
@@ -72,7 +72,7 @@ export function NavMenu() {
       </span>
       {open && (
         <div
-          class="absolute top-[120%] -right-4 z-[70] px-1 py-2 rounded border-1.5 border-current bg-white w-56 shadow overflow-hidden opacity-100 translate-y-0 transition"
+          class="absolute top-[120%] -right-4 z-[70] px-1 py-2 rounded border-1.5 border-current bg-white w-56 shadow overflow-hidden opacity-100 translate-y-0 transition [&>a]:rounded"
           dangerouslySetInnerHTML={{ __html: navMenuList.join("") }}
         />
       )}
