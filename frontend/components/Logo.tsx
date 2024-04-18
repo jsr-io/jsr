fa4893b4ca24eddc7b8dd8fa0d7d237b5e54b403
Max Duval <max.duval@theguardian.com>
2024-04-18 08:17:59 +0100

    refactor(logo): group letters
    more semantic, reduced colour repetition

--- frontend/components/Logo.tsx
+++ frontend/components/Logo.tsx
@@ -25,18 +25,11 @@ export function Logo(
           d="M0,2h2v-2h7v1h4v4h-2v2h-7v-1h-4"
           fill="#083344"
         />
-        <path
-          d="M1,3h1v1h1v-3h1v4h-3"
-          fill="#f7df1e"
-        />
-        <path
-          d="M5,1h3v1h-2v1h2v3h-3v-1h2v-1h-2"
-          fill="#f7df1e"
-        />
-        <path
-          d="M9,2h3v2h-1v-1h-1v3h-1"
-          fill="#f7df1e"
-        />
+        <g fill="#f7df1e">
+          <path d="M1,3h1v1h1v-3h1v4h-3" />
+          <path d="M5,1h3v1h-2v1h2v3h-3v-1h2v-1h-2" />
+          <path d="M9,2h3v2h-1v-1h-1v3h-1" />
+        </g>
       </svg>
     </>
   );
