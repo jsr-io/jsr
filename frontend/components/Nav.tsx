c2efbe20fee1785e42f38cd8fb109a59cfe1de4b
Luca Casonato <hello@lcas.dev>
2024-05-03 17:29:35 +0200

    fix: improve mobile nav bar
    This commit updates both styling and implementation of the mobile nav bar for package / scope / account page. This involves three main things:
    
    - align style between desktop and mobile nav bar
    - use an inline script tag to render the nav overflow menu to avoid a flashing shift of content
    - fallback to horizontal scrolling

--- frontend/components/Nav.tsx
+++ frontend/components/Nav.tsx
@@ -1,6 +1,6 @@
 // Copyright 2024 the JSR authors. All rights reserved. MIT license.
 import { ComponentChildren } from "preact";
-import { NavMenu } from "../islands/NavMenu.tsx";
+import { NavOverflow } from "./NavOverflow.tsx";
 
 export interface NavProps {
   children?: ComponentChildren;
@@ -12,12 +12,30 @@ export function Nav(props: NavProps) {
     <nav
       class={`${
         props.noTopMargin ? "" : "mt-3"
-      } md:border-b border-jsr-cyan-300/30 max-w-full flex justify-between overflow-hidden items-end`}
+      } border-b border-jsr-cyan-300/30 max-w-full flex justify-between overflow-x-auto items-end`}
     >
-      <ul id="nav-items" class="flex flex-row">
+      <style
+        dangerouslySetInnerHTML={{
+          __html:
+            "nav:has(#nav-items[data-unattached]) { visibility: hidden; }",
+        }}
+      />
+      <noscript>
+        <style
+          dangerouslySetInnerHTML={{
+            __html:
+              "nav:has(#nav-items[data-unattached]) { visibility: visible !important }",
+          }}
+        />
+      </noscript>
+      <ul
+        id="nav-items"
+        data-unattached
+        class="flex flex-row *:border-b-0 *:rounded-b-none"
+      >
         {props.children}
       </ul>
-      <NavMenu />
+      <NavOverflow />
     </nav>
   );
 }
@@ -31,11 +49,12 @@ export interface NavItemProps {
 export function NavItem(props: NavItemProps) {
   return (
     <a
-      class={`md:px-3 px-4 py-2 text-sm md:text-base min-h-10 leading-none rounded-md md:rounded-t-md md:rounded-b-none hover:bg-jsr-cyan-100 flex items-center select-none ${
+      class={`md:px-3 px-4 py-2 text-sm md:text-base min-h-10 leading-none rounded-md hover:bg-jsr-cyan-100 flex items-center select-none ${
         props.active
-          ? "bg-jsr-cyan-50 md:border-r-1 md:border-t-1 md:border-l-1 md:border-b-0 border-1 border-jsr-cyan-300/30 font-semibold"
+          ? "bg-jsr-cyan-50 border-1 border-jsr-cyan-300/30 font-semibold"
           : ""
       }`}
+      data-active={props.active ? "true" : undefined}
       href={props.href}
     >
       {props.children}
