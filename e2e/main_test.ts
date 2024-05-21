jsr-io/jsr

commit 0ea1d6b652fb19c45e7d2c06279c8de6d6fa2efd

chore: remove repeated URL parsing (#510)
Replaces #270

---------

Co-authored-by: Luca Casonato <hello@lcas.dev>

frontend/routes/badges/package.ts
@@ -31,12 +31,9 @@ export const handler: Handlers<unknown, State> = {
         });
       }
     } else {
-      const url = new URL(req.url);
-      url.protocol = "https:";
-
       const shieldsUrl = new URL("https://img.shields.io/endpoint");
-      shieldsUrl.search = url.search;
-      shieldsUrl.searchParams.set("url", url.href);
+      shieldsUrl.search = ctx.url.search;
+      shieldsUrl.searchParams.set("url", ctx.url.href);
       shieldsUrl.searchParams.set("logo", "jsr");
       shieldsUrl.searchParams.set("logoColor", "rgb(8,51,68)");
       shieldsUrl.searchParams.set("logoSize", "auto");

frontend/routes/badges/package_score.ts
@@ -35,12 +35,9 @@ export const handler: Handlers<unknown, State> = {
         });
       }
     } else {
-      const url = new URL(req.url);
-      url.protocol = "https:";
-
       const shieldsUrl = new URL("https://img.shields.io/endpoint");
-      shieldsUrl.search = url.search;
-      shieldsUrl.searchParams.set("url", url.href);
+      shieldsUrl.search = ctx.url.search;
+      shieldsUrl.searchParams.set("url", ctx.url.href);
       shieldsUrl.searchParams.set("logo", "jsr");
       shieldsUrl.searchParams.set("logoColor", "rgb(8,51,68)");
       shieldsUrl.searchParams.set("logoSize", "auto");

frontend/routes/badges/scope.ts
@@ -31,12 +31,9 @@ export const handler: Handlers<unknown, State> = {
         });
       }
     } else {
-      const url = new URL(req.url);
-      url.protocol = "https:";
-
       const shieldsUrl = new URL("https://img.shields.io/endpoint");
-      shieldsUrl.search = url.search;
-      shieldsUrl.searchParams.set("url", url.href);
+      shieldsUrl.search = ctx.url.search;
+      shieldsUrl.searchParams.set("url", ctx.url.href);
       shieldsUrl.searchParams.set("logo", "jsr");
       shieldsUrl.searchParams.set("logoColor", "rgb(8,51,68)");
       shieldsUrl.searchParams.set("logoSize", "auto");

frontend/routes/@[scope]/index.tsx
@@ -56,11 +56,10 @@ export default function ScopePackagesPage(
 }
 
 export const handler: Handlers<Data, State> = {
-  async GET(req, ctx) {
-    const url = new URL(req.url);
-    const page = +(url.searchParams.get("page") || 1);
+  async GET(_req, ctx) {
+    const page = +(ctx.url.searchParams.get("page") || 1);
     // Default to large enough to display all of @std.
-    const limit = +(url.searchParams.get("limit") || 50);
+    const limit = +(ctx.url.searchParams.get("limit") || 50);
 
     const [data, packagesResp, userInvitesResp] = await Promise.all([
       scopeDataWithMember(ctx.state, ctx.params.scope),

frontend/routes/_middleware.ts
@@ -12,11 +12,10 @@ export const tracer = new Tracer();
 
 const tracing: MiddlewareHandler<State> = async (req, ctx) => {
   ctx.state.span = tracer.spanForRequest(req, ctx.destination);
-  const url = new URL(req.url);
   const attributes: Record<string, string | bigint> = {
-    "http.url": url.href,
+    "http.url": ctx.url.href,
     "http.method": req.method,
-    "http.host": url.host,
+    "http.host": ctx.url.host,
   };
   const start = new Date();
   try {
@@ -26,15 +25,15 @@ const tracing: MiddlewareHandler<State> = async (req, ctx) => {
     return resp;
   } finally {
     const end = new Date();
-    ctx.state.span.record(url.pathname, start, end, attributes);
+    ctx.state.span.record(ctx.url.pathname, start, end, attributes);
   }
 };
 
 const auth: MiddlewareHandler<State> = async (req, ctx) => {
-  const url = new URL(req.url);
   const interactive =
     (ctx.destination === "route" || ctx.destination === "notFound") &&
-    !(url.pathname === "/gfm.css" || url.pathname === "/_frsh/client.js.map");
+    !(ctx.url.pathname === "/gfm.css" ||
+      ctx.url.pathname === "/_frsh/client.js.map");
   const { token, sudo } = getCookies(req.headers);
   if (interactive) {
     ctx.state.sudo = sudo === "1";
@@ -54,7 +53,7 @@ const auth: MiddlewareHandler<State> = async (req, ctx) => {
             span: ctx.state.span,
             token: null,
           });
-          const redirectTarget = `${url.pathname}${url.search}`;
+          const redirectTarget = `${ctx.url.pathname}${ctx.url.search}`;
           const loginUrl = `/login?redirect=${
             encodeURIComponent(redirectTarget)
           }`;

frontend/routes/admin/publishingTasks.tsx
@@ -101,11 +101,10 @@ export default function PublishingTasks({ data, url }: PageProps<Data>) {
 }
 
 export const handler: Handlers<Data, State> = {
-  async GET(req, ctx) {
-    const reqUrl = new URL(req.url);
-    const query = reqUrl.searchParams.get("search") || "";
-    const page = +(reqUrl.searchParams.get("page") || 1);
-    const limit = +(reqUrl.searchParams.get("limit") || 20);
+  async GET(_req, ctx) {
+    const query = ctx.url.searchParams.get("search") || "";
+    const page = +(ctx.url.searchParams.get("page") || 1);
+    const limit = +(ctx.url.searchParams.get("limit") || 20);
 
     const resp = await ctx.state.api.get<List<PublishingTask>>(
       path`/admin/publishing_tasks`,

frontend/routes/admin/scopes.tsx
@@ -40,10 +40,9 @@ export default function Scopes({ data, url }: PageProps<Data>) {
 
 export const handler: Handlers<Data, State> = {
   async GET(req, ctx) {
-    const reqUrl = new URL(req.url);
-    const query = reqUrl.searchParams.get("search") || "";
-    const page = +(reqUrl.searchParams.get("page") || 1);
-    const limit = +(reqUrl.searchParams.get("limit") || 20);
+    const query = ctx.url.searchParams.get("search") || "";
+    const page = +(ctx.url.searchParams.get("page") || 1);
+    const limit = +(ctx.url.searchParams.get("limit") || 20);
 
     const resp = await ctx.state.api.get<List<FullScope>>(path`/admin/scopes`, {
       query,

frontend/routes/admin/users.tsx
@@ -40,11 +40,10 @@ export default function Users({ data, url }: PageProps<Data>) {
 }
 
 export const handler: Handlers<Data, State> = {
-  async GET(req, ctx) {
-    const reqUrl = new URL(req.url);
-    const query = reqUrl.searchParams.get("search") || "";
-    const page = +(reqUrl.searchParams.get("page") || 1);
-    const limit = +(reqUrl.searchParams.get("limit") || 20);
+  async GET(_req, ctx) {
+    const query = ctx.url.searchParams.get("search") || "";
+    const page = +(ctx.url.searchParams.get("page") || 1);
+    const limit = +(ctx.url.searchParams.get("limit") || 20);
 
     const resp = await ctx.state.api.get<List<FullUser>>(path`/admin/users`, {
       query,

commit 27ff3af4de5108e3876f61ef16569b7e10ed4464

fix: broken badge urls (#540)
We passed an incorrect url to shields.io for generating the badges. 

This regression was introduced in
https://github.com/jsr-io/jsr/pull/510/files#diff-8caf5f85764d74fed531a1043245722b7f0fb15b3354df1ff3468742cd450ed7L35

Fixes https://github.com/jsr-io/jsr/issues/539

frontend/routes/badges/package.ts
@@ -31,9 +31,11 @@ export const handler: Handlers<unknown, State> = {
         });
       }
     } else {
+      const url = new URL("https://jsr.io" + ctx.url.pathname + ctx.url.search);
+
       const shieldsUrl = new URL("https://img.shields.io/endpoint");
-      shieldsUrl.search = ctx.url.search;
-      shieldsUrl.searchParams.set("url", ctx.url.href);
+      shieldsUrl.search = url.search;
+      shieldsUrl.searchParams.set("url", url.href);
       shieldsUrl.searchParams.set("logo", "jsr");
       shieldsUrl.searchParams.set("logoColor", "rgb(8,51,68)");
       shieldsUrl.searchParams.set("logoSize", "auto");

frontend/routes/badges/package_score.ts
@@ -35,13 +35,17 @@ export const handler: Handlers<unknown, State> = {
         });
       }
     } else {
+      const url = new URL("https://jsr.io" + ctx.url.pathname + ctx.url.search);
+
       const shieldsUrl = new URL("https://img.shields.io/endpoint");
-      shieldsUrl.search = ctx.url.search;
-      shieldsUrl.searchParams.set("url", ctx.url.href);
+      shieldsUrl.search = url.search;
+      shieldsUrl.searchParams.set("url", url.href);
       shieldsUrl.searchParams.set("logo", "jsr");
       shieldsUrl.searchParams.set("logoColor", "rgb(8,51,68)");
       shieldsUrl.searchParams.set("logoSize", "auto");
       shieldsUrl.searchParams.set("cacheSeconds", "300");
+
+      console.log(shieldsUrl.href);
 
       const res = await fetch(shieldsUrl);
 

frontend/routes/badges/scope.ts
@@ -31,9 +31,11 @@ export const handler: Handlers<unknown, State> = {
         });
       }
     } else {
+      const url = new URL("https://jsr.io" + ctx.url.pathname + ctx.url.search);
+
       const shieldsUrl = new URL("https://img.shields.io/endpoint");
-      shieldsUrl.search = ctx.url.search;
-      shieldsUrl.searchParams.set("url", ctx.url.href);
+      shieldsUrl.search = url.search;
+      shieldsUrl.searchParams.set("url", url.href);
       shieldsUrl.searchParams.set("logo", "jsr");
       shieldsUrl.searchParams.set("logoColor", "rgb(8,51,68)");
       shieldsUrl.searchParams.set("logoSize", "auto");

e2e/main_test.ts
@@ -1,5 +1,5 @@
 // Copyright 2024 the JSR authors. All rights reserved. MIT license.
-import { assertStringIncludes } from "./utils/assert.ts";
+import { assertNotMatch, assertStringIncludes } from "./utils/assert.ts";
 import { assertEquals } from "./utils/assert.ts";
 import { JSR_URL } from "./utils/configuration.ts";
 
@@ -322,4 +322,28 @@ if (import.meta.main) {
     response.headers.get("content-type"),
     "text/typescript",
   );
+});
+
+Deno.test("[GET /badges/@luca/flag] works", async (t) => {
+  await t.step("Package badge", async () => {
+    const res = await fetch(`${JSR_URL}badges/@luca/flag`);
+    const text = await res.text();
+    assertEquals(res.status, 200);
+    assertNotMatch(text, /please use https/);
+  });
+
+  // Skipping: These are not registered with shields.io yet
+  // await t.step("Package score badge", async () => {
+  //   const res = await fetch(`${JSR_URL}badges/@luca/flag/score`);
+  //   const text = await res.text();
+  //   assertEquals(res.status, 200);
+  //   assertNotMatch(text, /please use https/);
+  // });
+
+  // await t.step("Scope badge", async () => {
+  //   const res = await fetch(`${JSR_URL}badges/@luca`);
+  //   const text = await res.text();
+  //   assertEquals(res.status, 200);
+  //   assertNotMatch(text, /please use https/);
+  // });
 });
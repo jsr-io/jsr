27ff3af4de5108e3876f61ef16569b7e10ed4464
Marvin Hagemeister <marvin@deno.com>
2024-05-21 09:05:54 +0200

    fix: broken badge urls (#540)
    We passed an incorrect url to shields.io for generating the badges. 
    
    This regression was introduced in
    https://github.com/jsr-io/jsr/pull/510/files#diff-8caf5f85764d74fed531a1043245722b7f0fb15b3354df1ff3468742cd450ed7L35
    
    Fixes https://github.com/jsr-io/jsr/issues/539

--- frontend/routes/badges/package_score.ts
+++ frontend/routes/badges/package_score.ts
@@ -35,13 +35,17 @@ export const handler: Handlers<unknown, State> = {
         });
       }
     } else {
+      const url = new URL("https://jsr.io" + ctx.url.pathname + ctx.url.search);
+
       const shieldsUrl = new URL("https://img.shields.io/endpoint");
-      shieldsUrl.search = ctx.url.search;
-      shieldsUrl.searchParams.set("url", ctx.url.href);
+      shieldsUrl.search = url.search;
+      shieldsUrl.searchParams.set("url", url.href);
       shieldsUrl.searchParams.set("logo", "jsr");
       shieldsUrl.searchParams.set("logoColor", "rgb(8,51,68)");
       shieldsUrl.searchParams.set("logoSize", "auto");
       shieldsUrl.searchParams.set("cacheSeconds", "300");
+
+      console.log(shieldsUrl.href);
 
       const res = await fetch(shieldsUrl);
 
