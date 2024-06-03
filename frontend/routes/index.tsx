9004dcd01c26be33a28d071ec9a2fcf3a7e86a70
Tim Hårek Andreassen <tim@harek.no>
2024-06-03 11:28:45 +0200

    fix(a11y): Remove alt-texts from logos
    The logos are purely decroative and does not need to be read by a screen-reader
    
    Signed-off-by: Tim Hårek Andreassen <tim@harek.no>

--- frontend/routes/index.tsx
+++ frontend/routes/index.tsx
@@ -98,7 +98,7 @@ export default function Home({ data }: PageProps<Data>) {
           <img
             loading="lazy"
             src="/logos/typescript.svg"
-            alt="TypeScript logo"
+            alt=""
             class="w-full max-w-16 lg:max-w-36 lg:col-span-2 lg:mx-auto select-none"
             draggable={false}
           />
@@ -125,21 +125,21 @@ export default function Home({ data }: PageProps<Data>) {
             <img
               loading="lazy"
               src="/logos/npm.svg"
-              alt="npm logo"
+              alt=""
               class="w-full max-w-16 lg:max-w-28 select-none"
               draggable={false}
             />
             <img
               loading="lazy"
               src="/logos/yarn.svg"
-              alt="Yarn logo"
+              alt=""
               class="w-full max-w-16 lg:max-w-28 select-none"
               draggable={false}
             />
             <img
               loading="lazy"
               src="/logos/pnpm.svg"
-              alt="pnpm logo"
+              alt=""
               class="w-full max-w-16 lg:max-w-28 select-none"
               draggable={false}
             />
@@ -166,28 +166,28 @@ export default function Home({ data }: PageProps<Data>) {
             <img
               loading="lazy"
               src="/logos/node.svg"
-              alt="Node.js logo"
+              alt=""
               class="w-full max-w-9 lg:max-w-20 select-none"
               draggable={false}
             />
             <img
               loading="lazy"
               src="/logos/deno.svg"
-              alt="Deno logo"
+              alt=""
               class="w-full max-w-10 lg:max-w-20 select-none"
               draggable={false}
             />
             <img
               loading="lazy"
               src="/logos/bun.svg"
-              alt="Bun logo"
+              alt=""
               class="w-full max-w-11 lg:max-w-20 select-none"
               draggable={false}
             />
             <img
               loading="lazy"
               src="/logos/cloudflare-workers.svg"
-              alt="Cloudflare Workers logo"
+              alt=""
               class="w-full max-w-10 lg:max-w-20 select-none"
               draggable={false}
             />
