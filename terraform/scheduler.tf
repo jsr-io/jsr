c61f6e4750e0086be37f2e39eaaaa6b67f0f574e
Luca Casonato <hello@lcas.dev>
2024-07-16 14:10:00 +0200

    fix tf and add scheduler

--- terraform/scheduler.tf
+++ terraform/scheduler.tf
@@ -14,6 +14,21 @@ resource "google_cloud_scheduler_job" "npm_tarball_rebuild_missing" {
   }
 }
 
+resource "google_cloud_scheduler_job" "scrape_download_counts" {
+  name        = "scrape-download-counts"
+  description = "Scrape download counts from BigQuery and insert them into Postgres."
+  schedule    = "15 * * * *"
+  region      = "us-central1"
+
+  http_target {
+    http_method = "POST"
+    uri         = "${google_cloud_run_v2_service.registry_api_tasks.uri}/tasks/scrape_download_counts"
+    oidc_token {
+      service_account_email = google_service_account.task_dispatcher.email
+    }
+  }
+}
+
 resource "google_cloud_scheduler_job" "orama_deploy" {
   name        = "orama-deploy"
   description = "Deploy the Orama index with any new changes"
