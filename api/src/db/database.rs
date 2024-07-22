ed81836f9a1b64235bc3349c7f86b868c72228e1
Luca Casonato <hello@lcas.dev>
2024-07-22 15:28:29 +0200

    fix: improve grouping in Database::get_package_downloads_24h

--- api/src/db/database.rs
+++ api/src/db/database.rs
@@ -3124,9 +3124,10 @@ impl Database {
     sqlx::query_as!(
       DownloadDataPoint,
       r#"
-    SELECT time_bucket, kind as "kind: DownloadKind", count
+    SELECT time_bucket, kind as "kind: DownloadKind", SUM(count) as "count!"
     FROM version_download_counts_24h
     WHERE scope = $1 AND package = $2 AND time_bucket >= $3 AND time_bucket < $4
+    GROUP BY time_bucket, kind
     ORDER BY time_bucket ASC
     "#,
       scope as _,
