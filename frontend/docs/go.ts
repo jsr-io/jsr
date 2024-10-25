98c0ec3a42f6e444044b5ac41c94c7f4a81f0653
Luca Casonato <hello@lcas.dev>
2024-10-25 19:23:44 +0200

    chore: add meeting calendar link to /go (#784)

--- frontend/docs/go.ts
+++ frontend/docs/go.ts
@@ -4,6 +4,10 @@
 
 const staticRedirects = new Map([
   ["moderator", "https://forms.gle/MWLEi7Qj4cjvXoLb8"],
+  [
+    "meetings",
+    "https://calendar.google.com/calendar/u/0/embed?src=c_f678f0a1d6579ed92b162f756e928294efe657ba79d90bd864d048f0e2f70669@group.calendar.google.com",
+  ],
   ["use", "/docs/using-packages"],
   ["publish", "/docs/publishing-packages"],
   ["deno", "/docs/with/deno"],
