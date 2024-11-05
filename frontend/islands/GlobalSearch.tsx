fd49f83dccf3e794650a4c17f92622552f182464
crowlkats <crowlkats@toaxl.com>
2024-11-05 14:23:53 +0100

    fix: dont hide text on docs search

--- frontend/islands/GlobalSearch.tsx
+++ frontend/islands/GlobalSearch.tsx
@@ -262,7 +262,7 @@ export function GlobalSearch(
           <input
             type="search"
             name="search"
-            class={`w-full h-full search-input bg-white/90 !text-transparent !caret-black input rounded-r-none ${sizeClasses} relative`}
+            class={`w-full h-full search-input bg-white/90 ${kind === "packages" ? "!text-transparent" : ""} !caret-black input rounded-r-none ${sizeClasses} relative`}
             placeholder={placeholder}
             value={search.value}
             onInput={onInput}
