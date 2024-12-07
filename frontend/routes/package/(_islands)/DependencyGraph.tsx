ab50303bc345c4e89a90f364d94d92fbc2b54925
Michael Herzner <michael.b.herzner@gmail.com>
2024-12-07 23:47:00 +0100

    feat: add custom hook and controls to graph

--- frontend/routes/package/(_islands)/DependencyGraph.tsx
+++ frontend/routes/package/(_islands)/DependencyGraph.tsx
@@ -1,7 +1,15 @@
 // Copyright 2024 the JSR authors. All rights reserved. MIT license.
-import { useEffect, useRef } from "preact/hooks";
+import type { ComponentChildren } from "preact";
+import { useCallback, useEffect, useRef } from "preact/hooks";
 import { useSignal } from "@preact/signals";
 import { instance, type Viz } from "@viz-js/viz";
+import { ChevronDown } from "../../../components/icons/ChevronDown.tsx";
+import { ChevronLeft } from "../../../components/icons/ChevronLeft.tsx";
+import { ChevronRight } from "../../../components/icons/ChevronRight.tsx";
+import { ChevronUp } from "../../../components/icons/ChevronUp.tsx";
+import { Minus } from "../../../components/icons/Minus.tsx";
+import { Plus } from "../../../components/icons/Plus.tsx";
+import { Reset } from "../../../components/icons/Reset.tsx";
 
 interface DependencyGraphKindJsr {
   type: "jsr";
@@ -42,15 +50,14 @@ export interface DependencyGraphProps {
 }
 
 function createDigraph(dependencies: DependencyGraphProps["dependencies"]) {
-  return `
-digraph "dependencies" {
+  return `digraph "dependencies" {
   node [fontname="Courier", shape="box"]
 
-  ${
+${
     dependencies.map(({ children, dependency }, index) => {
       return [
-        [index, renderDependency(dependency)].join(" "),
-        ...children.map((child) => `${index} -> ${child}`),
+        `  ${index} ${renderDependency(dependency)}`,
+        ...children.map((child) => `  ${index} -> ${child}`),
       ].filter(Boolean).join("\n");
     }).join("\n")
   }
@@ -71,57 +78,161 @@ function renderDependency(dependency: DependencyGraphKind) {
   }
 }
 
-function renderJsrDependency(
-  dependency: DependencyGraphKindJsr,
-) {
+function renderJsrDependency(dependency: DependencyGraphKindJsr) {
   const label =
     `@${dependency.scope}/${dependency.package}@${dependency.version}`;
   const href = `/${label}`;
 
-  return `[href="${href}", label="${label}", tooltip="${label}"]\n`;
+  return `[href="${href}", label="${label}", tooltip="${label}"]`;
 }
 
 function renderNpmDependency(dependency: DependencyGraphKindNpm) {
   const label = `${dependency.package}@${dependency.version}`;
   const href = `https://www.npmjs.com/package/${dependency.package}`;
 
-  return `[href="${href}", label="${label}", tooltip="${label}"]\n`;
+  return `[href="${href}", label="${label}", tooltip="${label}"]`;
 }
 
 function renderRootDependency(dependency: DependencyGraphKindRoot) {
   const label = dependency.path;
 
-  return `[label="${label}", tooltip="${label}"]\n`;
+  return `[label="${label}", tooltip="${label}"]`;
 }
 
 function renderErrorDependency(dependency: DependencyGraphKindError) {
-  return ``;
+  const label = dependency.error;
+
+  return `[label="${label}", tooltip="${label}"]`;
 }
 
-export function DependencyGraph(props: DependencyGraphProps) {
-  const anchor = useRef<HTMLDivElement>(null);
+function useDigraph(dependencies: DependencyGraphProps["dependencies"]) {
+  const controls = useSignal({ pan: { x: 0, y: 0 }, zoom: 1 });
+  const ref = useRef<HTMLDivElement>(null);
+  const svg = useRef<SVGSVGElement | null>(null);
   const viz = useSignal<Viz | undefined>(undefined);
+
+  const pan = useCallback((x: number, y: number) => {
+    controls.value.pan.x += x;
+    controls.value.pan.y += y;
+    if (svg.current) {
+      svg.current.style.transform =
+        `translate(${controls.value.pan.x}px, ${controls.value.pan.y}px) scale(${controls.value.zoom})`;
+    }
+  }, [controls]);
+
+  const zoom = useCallback((zoom: number) => {
+    controls.value.zoom += zoom;
+    if (svg.current) {
+      svg.current.style.transform =
+        `translate(${controls.value.pan.x}px, ${controls.value.pan.y}px) scale(${controls.value.zoom})`;
+    }
+  }, [controls]);
+
+  const reset = useCallback(() => {
+    controls.value = { pan: { x: 0, y: 0 }, zoom: 1 };
+    if (svg.current) {
+      svg.current.style.transform = "";
+    }
+  }, []);
 
   useEffect(() => {
     (async () => {
       viz.value = await instance();
 
-      if (anchor.current && viz.value) {
-        const digraph = createDigraph(props.dependencies);
+      if (ref.current && viz.value) {
+        const digraph = createDigraph(dependencies);
 
-        console.log(digraph);
-
-        anchor.current.appendChild(
-          viz.value.renderSVGElement(digraph),
-        );
+        svg.current = viz.value.renderSVGElement(digraph);
+        ref.current.appendChild(svg.current);
       }
     })();
-  }, []);
+  }, [dependencies]);
+
+  return { pan, zoom, reset, ref };
+}
+
+interface GraphControlButtonProps {
+  children: ComponentChildren;
+  class: string;
+  onClick: () => void;
+  title: string;
+}
+
+function GraphControlButton(props: GraphControlButtonProps) {
+  return (
+    <button
+      aria-label={props.title}
+      class={`${props.class} bg-white text-jsr-gray-700 p-1.5 ring-1 ring-jsr-gray-700 rounded-full sm:rounded hover:bg-jsr-gray-100/30"`}
+      onClick={props.onClick}
+      title={props.title}
+    >
+      {props.children}
+    </button>
+  );
+}
+
+export function DependencyGraph(props: DependencyGraphProps) {
+  const { pan, zoom, reset, ref } = useDigraph(props.dependencies);
 
   return (
-    <div
-      class="-mx-4 md:mx-0 ring-1 ring-jsr-cyan-100 sm:rounded overflow-hidden"
-      ref={anchor}
-    />
+    <div class="-mx-4 md:mx-0 ring-1 ring-jsr-cyan-100 sm:rounded overflow-hidden relative">
+      <div ref={ref} />
+      <div class="absolute gap-1 grid grid-cols-3 bottom-4 right-4">
+        {/* zoom */}
+        <GraphControlButton
+          class="col-start-3 col-end-3 row-start-1 row-end-1"
+          onClick={() => zoom(0.1)}
+          title="Zoom in"
+        >
+          <Plus />
+        </GraphControlButton>
+        <GraphControlButton
+          class="col-start-3 col-end-3 row-start-3 row-end-3"
+          onClick={() => zoom(-0.1)}
+          title="Zoom out"
+        >
+          <Minus />
+        </GraphControlButton>
+
+        {/* pan */}
+        <GraphControlButton
+          class="col-start-2 col-end-2 row-start-1 row-end-1"
+          onClick={() => pan(0, 100)}
+          title="Pan up"
+        >
+          <ChevronUp />
+        </GraphControlButton>
+        <GraphControlButton
+          class="col-start-1 col-end-1 row-start-2 row-end-2"
+          onClick={() => pan(100, 0)}
+          title="Pan left"
+        >
+          <ChevronLeft />
+        </GraphControlButton>
+        <GraphControlButton
+          class="col-start-3 col-end-3 row-start-2 row-end-2"
+          onClick={() => pan(-100, 0)}
+          title="Pan right"
+        >
+          <ChevronRight />
+        </GraphControlButton>
+        <GraphControlButton
+          class="col-start-2 col-end-2 row-start-3 row-end-3"
+          onClick={() => pan(0, -100)}
+          title="Pan down"
+        >
+          <ChevronDown />
+        </GraphControlButton>
+
+        {/* reset */}
+        <GraphControlButton
+          class="col-start-2 col-end-2 row-start-2 row-end-2"
+          onClick={reset}
+          title="Reset view"
+        >
+          <Reset />
+        </GraphControlButton>
+      </div>
+    </div>
   );
 }
