5142d4dfc6aac21bc04925c83ed0863a75f812bd
EGAMAGZ <46827955+EGAMAGZ@users.noreply.github.com>
2025-01-16 16:19:31 -0600

    fix: Added validations to handle github api rate limiting and max retries

--- frontend/utils/github.ts
+++ frontend/utils/github.ts
@@ -6,7 +6,8 @@ export async function cachedGitHubLogin(user: User): Promise<string> {
   return await getOrInsertItem(
     `gh-login-${user.githubId}`,
     () => {
-      const fetchGithubUser = async () => {
+      const MAX_RETRIES = 3;
+      const fetchGithubUser = async (retryCount = 0) => {
         const response = await fetch(
           `https://api.github.com/user/${user.githubId}`,
           {
@@ -16,11 +17,19 @@ export async function cachedGitHubLogin(user: User): Promise<string> {
           },
         );
 
+        if(response.status === 403 && response.headers.get("x-ratelimit-remaining") === "0"){
+          throw new Error("Github API rate limit exceeded");
+        }
+
         const data = await response.json();
 
         if (!data.login) {
+          if(retryCount >= MAX_RETRIES){
+            throw new Error("Failed to fetch GitHub login after maximum retries")
+          }
+
           await new Promise((resolve) => setTimeout(resolve, 100));
-          return fetchGithubUser();
+          return fetchGithubUser(retryCount + 1);
         }
         return data.login;
       };
