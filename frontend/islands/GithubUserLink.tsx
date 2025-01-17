b72a1df6b48027432e8f1c5b16543559904f0ce2
EGAMAGZ <46827955+EGAMAGZ@users.noreply.github.com>
2025-01-16 18:58:02 -0600

    refactor: Added smaller font and italic to GithubUserLink's error message

--- frontend/islands/GithubUserLink.tsx
+++ frontend/islands/GithubUserLink.tsx
@@ -23,7 +23,7 @@ export function GitHubUserLink({ user }: { user?: User }) {
   });
 
   if (error.value) {
-    return <span>Could not load GitHub username</span>;
+    return <span class="italic text-[0.625rem]">Could not load GitHub username</span>
   }
 
   return login.value == ""
