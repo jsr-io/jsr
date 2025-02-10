9bc0eadc1e71e411808a7d5815f76f9ffb46bcd8
Augustin Mauroy <97875033+AugustinMauroy@users.noreply.github.com>
2025-02-10 10:41:42 +0100

    feat(ui): add scope naming policy link (#942)
    fix #900 + giving little bit more space between text and button because
    shadow of the button was to close to the text.
    
    <img width="1429" alt="Capture d’écran 2025-02-07 à 16 18 51"
    src="https://github.com/user-attachments/assets/4a560bdc-01f9-42aa-849f-13fd38f6be91"
    />
    <img width="1429" alt="Capture d’écran 2025-02-07 à 16 18 56"
    src="https://github.com/user-attachments/assets/e135082c-af6b-4d88-9dad-d8037a56813f"
    />

--- frontend/islands/new.tsx
+++ frontend/islands/new.tsx
@@ -87,7 +87,7 @@ export function ScopeSelect(
           locked={locked}
         />
         {!locked && (
-          <p>
+          <p class="mt-2">
             or{" "}
             <button
               class="inline link"
@@ -102,6 +102,12 @@ export function ScopeSelect(
           more scope{scopesLeft !== 1 && "s"}.{" "}
           <a href="/account/settings" class="link">View quotas</a> or{" "}
           <a href="/account" class="link">manage your scopes</a>.
+        </p>
+        <p class="text-jsr-gray-700 text-sm">
+          Before creating a new scope, please read the{" "}
+          <a href="/docs/usage-policy#scope-names" class="link">
+            scope naming policy
+          </a>.
         </p>
       </>
     );
