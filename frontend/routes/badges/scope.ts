9f70d028fa714b560fdb741aa1e0d4ab66357293
EGAMAGZ <46827955+EGAMAGZ@users.noreply.github.com>
2025-04-25 23:34:07 -0600

    refactor: Attemp of the usage of badge-maker

--- frontend/routes/badges/scope.ts
+++ frontend/routes/badges/scope.ts
@@ -4,19 +4,20 @@ import { accepts } from "@std/http/negotiation";
 import { define } from "../../util.ts";
 import { Scope } from "../../utils/api_types.ts";
 import { path } from "../../utils/api.ts";
+import { makeBadge } from "badge-maker";
 
 export const handler = define.handlers({
   async GET(ctx) {
     const req = ctx.req;
 
+    const scopeResp = await ctx.state.api.get<Scope>(
+      path`/scopes/${ctx.params.scope}`,
+    );
+
     if (
       accepts(req, "application/json", "text/html", "image/*") ===
         "application/json"
     ) {
-      const scopeResp = await ctx.state.api.get<Scope>(
-        path`/scopes/${ctx.params.scope}`,
-      );
-
       if (!scopeResp.ok) {
         if (scopeResp.code === "scopeNotFound") {
           return new Response(null, { status: 404 });
@@ -33,29 +34,36 @@ export const handler = define.handlers({
         });
       }
     } else {
-      const url = new URL("https://jsr.io" + ctx.url.pathname + ctx.url.search);
+      let badge: string;
 
-      const shieldsUrl = new URL("https://img.shields.io/endpoint");
-      shieldsUrl.search = url.search;
-      shieldsUrl.searchParams.set("url", url.href);
-      shieldsUrl.searchParams.set("logo", "jsr");
-      shieldsUrl.searchParams.set("logoSize", "auto");
-      shieldsUrl.searchParams.set("cacheSeconds", "300");
-
-      if (!ctx.url.searchParams.has("logoColor")) {
-        shieldsUrl.searchParams.set("logoColor", "rgb(8,51,68)");
+      if (!scopeResp.ok) {
+        if (scopeResp.code === "scopeNotFound") {
+          badge = makeBadge({
+            label: "custom badge",
+            message: "resource not found",
+            color: "rgb(206,88,66)",
+          });
+        } else {
+          throw scopeResp;
+        }
+      } else {
+        const url = new URL(
+          "https://jsr.io" + ctx.url.pathname + ctx.url.search,
+        );
+        badge = makeBadge({
+          label: "",
+          message: `@${scopeResp.data.scope}`,
+          labelColor: "rgb(247,223,30)",
+          color: "rgb(8,51,68)",
+          links: [url.toString()],
+        });
       }
 
-      const res = await fetch(shieldsUrl);
-
-      return new Response(res.body, {
-        status: res.status,
+      return new Response(badge, {
         headers: {
-          "access-control-allow-origin": res.headers.get(
-            "access-control-allow-origin",
-          )!,
-          "cache-control": res.headers.get("cache-control")!,
-          "content-type": res.headers.get("content-type")!,
+          "access-control-allow-origin": "*",
+          "cache-control": "max-age=300,s-maxage=300",
+          "content-type": "image/svg+xml;charset=utf-8",
         },
       });
     }
