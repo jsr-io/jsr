a37ff70c35a058ba6a1ecd826c997708828067e9
EGAMAGZ <46827955+EGAMAGZ@users.noreply.github.com>
2025-04-28 11:42:13 -0600

    feat: Attempt to load svg

--- frontend/routes/badges/scope.ts
+++ frontend/routes/badges/scope.ts
@@ -6,6 +6,13 @@ import { Scope } from "../../utils/api_types.ts";
 import { path } from "../../utils/api.ts";
 import { makeBadge } from "badge-maker";
 
+const SVG_LOGO = `\
+<svg fill="rgb(8,51,68)" role="img" viewBox="0 0 24 12.924000000000003" xmlns="http://www.w3.org/2000/svg">
+<title>JSR</title>
+<path d="M3.692 0v3.693H0v7.384h7.385v1.847h12.923v-3.693H24V1.847h-7.385V0Zm1.846 1.847h1.847v7.384H1.846v-3.692h1.846v1.846h1.846z m3.693 0h5.538V3.692h-3.692v1.846h3.692v5.538H9.231V9.232h3.692v-1.846H9.231Zm7.384 1.846h5.539v3.692h-1.846v-1.846h-1.846v5.538h-1.847z"/>
+</svg>
+`;
+
 export const handler = define.handlers({
   async GET(ctx) {
     const req = ctx.req;
@@ -56,6 +63,7 @@ export const handler = define.handlers({
           labelColor: "rgb(247,223,30)",
           color: "rgb(8,51,68)",
           links: [url.toString()],
+	  logoBase64: `data:image/svg+xml,${SVG_LOGO}`
         });
       }
 
