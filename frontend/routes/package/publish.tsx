2e25c01125972fe7249485e2b3411664bdf6234c
EGAMAGZ <46827955+EGAMAGZ@users.noreply.github.com>
2025-04-29 14:25:09 -0600

    Revert "style: Formatted code"
    This reverts commit c569da7475984002b2086ec45bc096d3d8ae8e9f.

--- frontend/routes/package/publish.tsx
+++ frontend/routes/package/publish.tsx
@@ -266,7 +266,10 @@ function GitHubActions({ pkg, canEdit, user }: {
       </div>
       <pre class="bg-slate-900 text-white rounded-lg rounded-tl-none p-4 mb-2 w-full max-w-full overflow-auto relative">
       	<div class="bg-white dark:bg-jsr-gray-900 text-white rounded p-0.5 absolute top-2 right-2 z-1 size-8 flex justify-center items-center">
-	  <CopyButton text={WORKFLOW_CODE.replace(/<[^>]+>/g, '')} title="Copy workflow code" />
+		<CopyButton
+		  text={WORKFLOW_CODE.replace(/<[^>]+>/g, '')}
+		  title="Copy workflow code"
+		/>
 	</div>
         <code
           dangerouslySetInnerHTML={{__html: WORKFLOW_CODE}}>
