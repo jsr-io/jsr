da105b3fe6254fa631b9eee719edb3e8db795938
Leo Kettmeir <crowlkats@toaxl.com>
2026-01-26 14:13:37 +0100

    add old records back and remove ns record

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -10,18 +10,6 @@ resource "google_dns_managed_zone" "default" {
   description = "Managed zone for ${var.domain_name}"
 }
 
-data "cloudflare_zone" "zone" {
-  zone_id = var.cloudflare_zone_id
-}
-
-resource "google_dns_record_set" "ns" {
-  name         = "${var.domain_name}."
-  managed_zone = google_dns_managed_zone.default.name
-  type         = "NS"
-  ttl          = 300
-  rrdatas      = [for record in data.cloudflare_zone.zone.name_servers : "${record}."]
-}
-
 resource "cloudflare_dns_record" "a" {
   zone_id = var.cloudflare_zone_id
   name    = "@"
@@ -74,4 +62,53 @@ resource "cloudflare_dns_record" "npm_aaaa" {
   content = google_compute_global_address.frontend_ipv6.address
   proxied = true
   ttl     = 1
+}
+
+// old records
+resource "google_dns_record_set" "a" {
+  name         = "${var.domain_name}."
+  managed_zone = google_dns_managed_zone.default.name
+  type         = "A"
+  ttl          = 300
+  rrdatas      = [google_compute_global_address.frontend_ipv4.address]
+}
+
+resource "google_dns_record_set" "aaaa" {
+  name         = "${var.domain_name}."
+  managed_zone = google_dns_managed_zone.default.name
+  type         = "AAAA"
+  ttl          = 300
+  rrdatas      = [google_compute_global_address.frontend_ipv6.address]
+}
+
+resource "google_dns_record_set" "api_a" {
+  name         = "${local.api_domain}."
+  managed_zone = google_dns_managed_zone.default.name
+  type         = "A"
+  ttl          = 300
+  rrdatas      = [google_compute_global_address.frontend_ipv4.address]
+}
+
+resource "google_dns_record_set" "api_aaaa" {
+  name         = "${local.api_domain}."
+  managed_zone = google_dns_managed_zone.default.name
+  type         = "AAAA"
+  ttl          = 300
+  rrdatas      = [google_compute_global_address.frontend_ipv6.address]
+}
+
+resource "google_dns_record_set" "npm_a" {
+  name         = "${local.npm_domain}."
+  managed_zone = google_dns_managed_zone.default.name
+  type         = "A"
+  ttl          = 300
+  rrdatas      = [google_compute_global_address.frontend_ipv4.address]
+}
+
+resource "google_dns_record_set" "npm_aaaa" {
+  name         = "${local.npm_domain}."
+  managed_zone = google_dns_managed_zone.default.name
+  type         = "AAAA"
+  ttl          = 300
+  rrdatas      = [google_compute_global_address.frontend_ipv6.address]
 }
