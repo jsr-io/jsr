2bdc1f4445f00043784006e303407b4619497b6a
Leo Kettmeir <crowlkats@toaxl.com>
2026-01-28 18:59:03 +0100

    debug

--- lb/analytics.ts
+++ lb/analytics.ts
@@ -4,7 +4,7 @@ import type { WorkerEnv } from "./types.ts";
 
 export function trackJSRDownload(pathname: string, env: WorkerEnv): void {
   const match = pathname.match(/^\/@([^/]+)\/([^/]+)\/([^/]+)_meta\.json$/);
-  console.log(match === null ? "no match" : "matched download:", match);
+  console.log(pathname, match === null ? "no match" : "matched download:", match);
   if (match) {
     const [, scope, packageName, version] = match;
     console.log("tracked download:", [
