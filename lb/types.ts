7a304fe00cdc95c3294e34ce26e1bf73e4edf2d4
Leo Kettmeir <crowlkats@toaxl.com>
2026-01-30 13:27:12 +0100

    refactor: switch to cloudflare workers for loadbalancing  (#1255)

---
+++ lb/types.ts
@@ -0,0 +1,41 @@
+// Copyright 2024 the JSR authors. All rights reserved. MIT license.
+
+declare global {
+  interface AnalyticsEngineDataset {
+    writeDataPoint(event?: AnalyticsEngineDataPoint): void;
+  }
+
+  interface AnalyticsEngineDataPoint {
+    indexes?: [string];
+    doubles?: number[];
+    blobs?: string[];
+  }
+
+  interface CacheStorage {
+    default: Cache;
+  }
+
+  interface RequestInit {
+    cf?: {
+      cacheEverything?: boolean;
+      cacheTtl?: number;
+      cacheKey?: string;
+    };
+  }
+}
+
+export interface WorkerEnv {
+  REGISTRY_API_URL: string;
+
+  REGISTRY_FRONTEND_URL: string;
+
+  GCS_ENDPOINT?: string;
+  MODULES_BUCKET: string;
+  NPM_BUCKET: string;
+
+  ROOT_DOMAIN: string;
+  API_DOMAIN: string;
+  NPM_DOMAIN: string;
+
+  DOWNLOADS?: AnalyticsEngineDataset;
+}
