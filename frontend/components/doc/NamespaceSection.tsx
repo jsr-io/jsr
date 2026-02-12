3815b5afd6b1caf4e87735d83f6840e696afe318
Leo Kettmeir <crowlkats@toaxl.com>
2026-02-12 22:23:05 +0100

    fix

--- frontend/components/doc/NamespaceSection.tsx
+++ frontend/components/doc/NamespaceSection.tsx
@@ -4,13 +4,11 @@ import { DocNodeKindIcon } from "./DocNodeKindIcon.tsx";
 
 export function NamespaceSection({ items }: { items: NamespaceNodeCtx[] }) {
   return (
-    <div class="!mt-6 max-w-prose">
-      {items.map((item, i) => (
+    <div class="space-y-2 !mt-6 max-w-prose">
+      {items.map((item) => (
         <div
           id={item.id}
-          class={`flex gap-x-2.5 md:min-h-[4rem] lg:pr-4 p-2 ${
-            i === 0 ? "" : "mt-2"
-          } -m-2 min-h-12 rounded transition duration-125 hover:bg-jsr-gray-100 dark:hover:bg-jsr-gray-900/50 ${
+          class={`flex gap-x-2.5 md:min-h-[4rem] lg:pr-4 rounded transition duration-125 ${
             item.deprecated ? "opacity-60" : ""
           }`}
           aria-label={item.deprecated ? "deprecated" : undefined}
@@ -27,27 +25,29 @@ export function NamespaceSection({ items }: { items: NamespaceNodeCtx[] }) {
                 : ""
             }`}
           >
-            <div>
-              <a href={item.href} title={item.name} class="block font-mono">
-                <span class="highlightable leading-none break-all font-medium">
-                  {item.name}
-                </span>
-                {item.ty && (
-                  <>
-                    <span
-                      class="font-light opacity-85 dark:opacity-75"
-                      // jsdoc rendering
-                      // deno-lint-ignore react-no-danger
-                      dangerouslySetInnerHTML={{ __html: item.ty.ty }}
-                    />
-                    {item.ty.info && (
-                      <div class="italic text-xs ml-2 text-stone-600 dark:text-stone-400">
-                        {item.ty.info}
-                      </div>
-                    )}
-                  </>
-                )}
+            <div class="block font-mono">
+              <a
+                href={item.href}
+                title={item.name}
+                class="highlightable leading-none break-all font-medium underline underline-offset-2"
+              >
+                {item.name}
               </a>
+              {item.ty && (
+                <>
+                  <span
+                    class="font-light opacity-85 dark:opacity-75"
+                    // jsdoc rendering
+                    // deno-lint-ignore react-no-danger
+                    dangerouslySetInnerHTML={{ __html: item.ty.ty }}
+                  />
+                  {item.ty.info && (
+                    <div class="italic text-xs ml-2 text-stone-600 dark:text-stone-400">
+                      {item.ty.info}
+                    </div>
+                  )}
+                </>
+              )}
             </div>
 
             <div class="mt-2 text-sm leading-5">
@@ -71,8 +71,14 @@ export function NamespaceSection({ items }: { items: NamespaceNodeCtx[] }) {
               <ul class="gap-y-3 text-sm mt-3 ml-2">
                 {item.subitems.map((subitem) => (
                   <li>
-                    <a href={subitem.href} class="block font-mono">
-                      <span class="highlightable">{subitem.title}</span>
+                    <div class="block font-mono">
+                      <a
+                        href={subitem.href}
+                        title={subitem.title}
+                        class="highlightable underline underline-offset-2"
+                      >
+                        {subitem.title}
+                      </a>
                       {subitem.ty && (
                         <>
                           <span
@@ -88,7 +94,7 @@ export function NamespaceSection({ items }: { items: NamespaceNodeCtx[] }) {
                           )}
                         </>
                       )}
-                    </a>
+                    </div>
 
                     <div class="mt-2 leading-5">
                       {subitem.docs
