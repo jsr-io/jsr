416ef8726a92e0f86b17bc815d8fb7bad61c9d74
Leo Kettmeir <crowlkats@toaxl.com>
2026-02-19 03:40:48 +0100

    undo docblock subtitles

--- frontend/components/doc/DocBlockSubtitleClass.tsx
+++ frontend/components/doc/DocBlockSubtitleClass.tsx
@@ -7,47 +7,6 @@ export function DocBlockSubtitleClass(
 ) {
   return (
     <>
-      {value.is_abstract_change && (
-        <>
-          {value.is_abstract_change.old && (
-            <div class={`diff-removed rounded px-1 py-0.5 inline-block`}>
-              <span class="text-stone-400 italic dark:text-stone-500">
-                {"abstract"}
-              </span>
-            </div>
-          )}
-          {value.is_abstract_change.new && (
-            <div
-              class={`diff-added rounded px-1 py-0.5 inline-block`}
-            >
-              <span class="text-stone-400 italic dark:text-stone-500">
-                {"abstract"}
-              </span>
-            </div>
-          )}
-        </>
-      )}
-
-      {value.implements_removed && value.implements_removed.length > 0 && (
-        <div class={`diff-removed rounded px-1 py-0.5`}>
-          <span class="text-stone-400 italic dark:text-stone-500">
-            {" implements "}
-          </span>
-          {value.implements_removed.map((impl, index) => (
-            <>
-              <span
-                // includes type defs which are generated with spans
-                // deno-lint-ignore react-no-danger
-                dangerouslySetInnerHTML={{ __html: impl }}
-              />
-              {index < value.implements_removed!.length - 1 && (
-                <span>{", "}</span>
-              )}
-            </>
-          ))}
-        </div>
-      )}
-
       {value.implements && value.implements.length > 0 && (
         <div>
           <span class="text-stone-400 italic dark:text-stone-500">
@@ -56,9 +15,6 @@ export function DocBlockSubtitleClass(
           {value.implements.map((impl, index) => (
             <>
               <span
-                class={value.implements_added?.includes(impl)
-                  ? `diff-added rounded px-0.5`
-                  : ""}
                 // includes type defs which are generated with spans
                 // deno-lint-ignore react-no-danger
                 dangerouslySetInnerHTML={{ __html: impl }}
@@ -68,91 +24,25 @@ export function DocBlockSubtitleClass(
           ))}
         </div>
       )}
-
-      {value.extends_change
-        ? (
-          <>
-            {value.extends_change.old !== null && (
-              <div class={`diff-removed rounded px-1 py-0.5`}>
-                <span class="text-stone-400 italic dark:text-stone-500">
-                  {" extends "}
-                </span>
-                <span
-                  // includes type defs which are generated with spans
-                  // deno-lint-ignore react-no-danger
-                  dangerouslySetInnerHTML={{ __html: value.extends_change.old }}
-                />
-              </div>
-            )}
-            {value.extends_change.new !== null && (
-              <div class={`diff-added rounded px-1 py-0.5`}>
-                <span class="text-stone-400 italic dark:text-stone-500">
-                  {" extends "}
-                </span>
-                <span
-                  // includes type defs which are generated with spans
-                  // deno-lint-ignore react-no-danger
-                  dangerouslySetInnerHTML={{ __html: value.extends_change.new }}
-                />
-              </div>
-            )}
-          </>
-        )
-        : value.extends && (
-          <div>
-            <span class="text-stone-400 italic dark:text-stone-500">
-              {" extends "}
-            </span>
-            {value.extends.href
-              ? (
-                <a class="link" href={value.extends.href}>
-                  {value.extends.symbol}
-                </a>
-              )
-              : <span>{value.extends.symbol}</span>}
-            {value.extends.type_args && (
-              <span
-                // includes type defs which are generated with spans
-                // deno-lint-ignore react-no-danger
-                dangerouslySetInnerHTML={{ __html: value.extends.type_args }}
-              />
-            )}
-          </div>
-        )}
-
-      {value.super_type_params_removed &&
-        value.super_type_params_removed.length > 0 && (
-        <div class={`diff-removed rounded px-1 py-0.5`}>
-          {value.super_type_params_removed.map((param, index) => (
-            <>
-              <span
-                // includes type defs which are generated with spans
-                // deno-lint-ignore react-no-danger
-                dangerouslySetInnerHTML={{ __html: param }}
-              />
-              {index < value.super_type_params_removed!.length - 1 && (
-                <span>{", "}</span>
-              )}
-            </>
-          ))}
-        </div>
-      )}
-
-      {value.super_type_params_added &&
-        value.super_type_params_added.length > 0 && (
-        <div class={`diff-added rounded px-1 py-0.5`}>
-          {value.super_type_params_added.map((param, index) => (
-            <>
-              <span
-                // includes type defs which are generated with spans
-                // deno-lint-ignore react-no-danger
-                dangerouslySetInnerHTML={{ __html: param }}
-              />
-              {index < value.super_type_params_added!.length - 1 && (
-                <span>{", "}</span>
-              )}
-            </>
-          ))}
+      {value.extends && (
+        <div>
+          <span class="text-stone-400 italic dark:text-stone-500">
+            {" extends "}
+          </span>
+          {value.extends.href
+            ? (
+              <a class="link" href={value.extends.href}>
+                {value.extends.symbol}
+              </a>
+            )
+            : <span>{value.extends.symbol}</span>}
+          {value.extends.type_args && (
+            <span
+              // includes type defs which are generated with spans
+              // deno-lint-ignore react-no-danger
+              dangerouslySetInnerHTML={{ __html: value.extends.type_args }}
+            />
+          )}
         </div>
       )}
     </>
--- frontend/components/doc/DocBlockSubtitleInterface.tsx
+++ frontend/components/doc/DocBlockSubtitleInterface.tsx
@@ -5,53 +5,25 @@ import type { DocBlockSubtitleInterfaceCtx } from "@deno/doc/html-types";
 export function DocBlockSubtitleInterface(
   { subtitle: { value } }: { subtitle: DocBlockSubtitleInterfaceCtx },
 ) {
-  const hasRemovals = value.extends_removed && value.extends_removed.length > 0;
-  const hasExtends = value.extends && value.extends.length > 0;
-
-  if (!hasRemovals && !hasExtends) {
+  if (!value.extends || value.extends.length === 0) {
     return null;
   }
 
   return (
-    <>
-      {hasRemovals && (
-        <div class={`diff-removed rounded px-1 py-0.5`}>
-          <span class="text-stone-400 italic dark:text-stone-500">
-            {" extends "}
-          </span>
-          {value.extends_removed!.map((ext, index) => (
-            <>
-              <span
-                // includes type defs which are generated with spans
-                // deno-lint-ignore react-no-danger
-                dangerouslySetInnerHTML={{ __html: ext }}
-              />
-              {index < value.extends_removed!.length - 1 && <span>{", "}</span>}
-            </>
-          ))}
-        </div>
-      )}
-
-      {hasExtends && (
-        <div>
-          <span class="text-stone-400 italic dark:text-stone-500">
-            {" extends "}
-          </span>
-          {value.extends.map((ext, index) => (
-            <>
-              <span
-                class={value.extends_added?.includes(ext)
-                  ? `diff-added rounded px-0.5`
-                  : ""}
-                // includes type defs which are generated with spans
-                // deno-lint-ignore react-no-danger
-                dangerouslySetInnerHTML={{ __html: ext }}
-              />
-              {index < value.extends!.length - 1 && <span>{", "}</span>}
-            </>
-          ))}
-        </div>
-      )}
-    </>
+    <div>
+      <span class="text-stone-400 italic dark:text-stone-500">
+        {" extends "}
+      </span>
+      {value.extends.map((ext, index) => (
+        <>
+          <span
+            // includes type defs which are generated with spans
+            // deno-lint-ignore react-no-danger
+            dangerouslySetInnerHTML={{ __html: ext }}
+          />
+          {index < value.extends!.length - 1 && <span>{", "}</span>}
+        </>
+      ))}
+    </div>
   );
 }
