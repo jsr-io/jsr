951e09bbb3ebb68ce6666b20963fe5a59ac78563
Leo Kettmeir <crowlkats@toaxl.com>
2026-02-19 19:15:37 +0100

    comment IDs

--- terraform/buckets.tf
+++ terraform/buckets.tf
@@ -36,8 +36,8 @@ resource "cloudflare_account_token" "r2_publishing" {
   policies = [{
     effect = "allow"
     permission_groups = [
-      { id = "6a018a9f2fc74eb6b293b0c548f38b39" },
-      { id = "2efd5506f9c8494dacb1fa10a3e7d5b6" },
+      { id = "6a018a9f2fc74eb6b293b0c548f38b39" }, // Workers R2 Storage Bucket Item Read
+      { id = "2efd5506f9c8494dacb1fa10a3e7d5b6" }, // Workers R2 Storage Bucket Item Write
     ]
     resources = jsonencode({
       "com.cloudflare.edge.r2.bucket.${var.cloudflare_account_id}_default_${cloudflare_r2_bucket.publishing.name}" = "*"
