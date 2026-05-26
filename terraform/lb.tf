35c7b7bbc4457fcf8ffb9042c01428de4197e509
Leo Kettmeir <crowlkats@toaxl.com>
2026-05-26 09:41:18 +0200

    fix: increase ratelimit

--- terraform/lb.tf
+++ terraform/lb.tf
@@ -63,7 +63,7 @@ resource "cloudflare_workers_script" "jsr_lb" {
       name         = "FRONTEND_RATELIMIT"
       namespace_id = "1001"
       simple = {
-        limit  = 30
+        limit  = 60
         period = 60
       }
     }
